8a17f9406ad59008ed0509a5688c1cd4bb24b84f
lincvv <lincv@fastmail.com>
2020-08-08 17:16:32 +0300

    feat(start fix misc)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import binascii
-
+import mmap
 
 class Dump:
 
@@ -7,6 +7,8 @@ class Dump:
         self.path_dump = name_dump
         self.sn_full_bytes = bytearray()
         self.dump_full = None
+        self.misc_b = bytearray(b'\x4E\x56\x41\x52\xEA\x02\xFF\xFF\xFF')
+        self.misc_t = bytearray(b'\x4E\x56\x41\x52\x8A\x00\xFF\xFF\xFF')
 
     def __repr__(self):
         return self.path_dump
@@ -38,10 +40,31 @@ class Dump:
             file.write(self.dump_full.replace(self.sn_full_bytes, word_byte))
 
     def _get_data(self, *, offset: int, count: int) -> bytes:
-        # count -> number of bytes read
-        # offset -> read start offset
+        """
+
+        :param offset: read start offset
+        :param count: number of bytes read
+        :return:
+        """
+
         with open(self.path_dump, "rb") as file:
             self.dump_full = file.read()
+            # print(self.misc_t)
+            # mm = mmap.mmap(file.fileno(), 0)
+            offset_t = self.dump_full.find(self.misc_t, 0)
+            offset_b = self.dump_full.find(self.misc_b, 0)
+            print(hex(offset_t))
+            print(bool(offset_b))
+            # print(mm.find(s_byte, 0))
+            file.seek(offset_t)
+            misk_t = file.read(int('8A', 16))
+            # file.seek(offset_b)
+            # misk_b = file.read(int('2EA', 16))
+            with open(self.path_dump, 'wb') as _f:
+                _f.write(self.dump_full.replace(misk_t, misk_t))
+            # print(hex(_f))
+            print(misk_t)
+            # print(misk_b)
 
             file.seek(offset)
             data = file.read(count)
