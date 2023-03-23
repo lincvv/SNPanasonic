268fbcbec87f8c2162b0b11a2fb69530d321e8ba
lincvv <lincv@protonmail.com>
2023-03-23 16:44:44 +0200

    fix(setup.py locale)

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ os.environ['TK_LIBRARY'] = "C:\\Python311\\tcl\\tk8.6"
 
 include_files = ["img", "data", "uefitool", r"C:\Python311\DLLs\tcl86t.dll", r"C:\Python311\DLLs\tk86t.dll", "fit11"]
 excludes = ['unittest', 'email', 'html', 'http', 'xml', 'pydoc', 'doctest', 'argparse',
-            'zipfile', 'pickle', 'locale', 'calendar', 'base64', 'gettext', 'bz2', 'getopt',
+            'zipfile', 'pickle', 'calendar', 'base64', 'gettext', 'bz2', 'getopt',
             'stringprep', 'quopri', 'copy', 'imp']
-packages = ['os', 'binascii', 'mmap', 'subprocess', 'logging', 'webbrowser', 'pathlib', 'time', 'datetime', 'urllib']
+packages = ['os', 'binascii', 'mmap', 'subprocess', 'logging', 'webbrowser', 'pathlib', 'time', 'datetime', 'urllib', 'locale']
 
 setup(
     name="PanasonicTool",
@@ -22,7 +22,7 @@ setup(
         'excludes': excludes,
         'include_msvcr': True,
     }},
-    executables=[Executable("app.py", base="Win32GUI")]
+    executables=[Executable("PanasonicTool.py", base="Win32GUI")]
 )
 
 # python setup.py bdist_msi
